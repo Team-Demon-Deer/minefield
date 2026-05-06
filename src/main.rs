b497642ac685db35e2f7c2675dcebf102653e956
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-05-06 17:43:38 -0400

    Progress

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,69 @@
-use std::ops::Range;
-
 use bevy::{
     color::palettes::tailwind::RED_400,
     image::{ImageArrayLayout, ImageLoaderSettings},
     input::mouse::AccumulatedMouseScroll,
+    math::bounding::Aabb2d,
     prelude::*,
     sprite_render::{TileData, TilemapChunk, TilemapChunkTileData},
 };
 use rand::{RngExt, SeedableRng};
 use rand_chacha::ChaCha8Rng;
+use std::{
+    mem::transmute,
+    ops::{Index, Range},
+};
+use time::UtcDateTime;
 
-/// Player movement speed factor.
-const PLAYER_SPEED: f32 = 100.;
+/// Game Cursor movement speed factor.
+const GAMECURSOR_SPEED: f32 = 100.;
 
 /// How quickly should the camera snap to the desired location.
 const CAMERA_DECAY_RATE: f32 = 2.;
 const CAMERA_ZOOM_SPEED: f32 = 0.5;
 const CAMERA_ZOOM_RANGE: Range<f32> = 0.1..10.0;
+const CELL_SIZE: usize = 16;
+
+const RANDOM_SEED: u64 = 34;
+
+#[derive(Component)]
+struct Cell {
+    logical_position: LogicalPosition,
+    state: CellState,
+    bomb_locations: TilesArray,
+}
+
+#[derive(Debug)]
+struct LogicalPosition {
+    x: i64,
+    y: i64,
+}
+
+enum CellState {
+    Fresh,
+    InProgress {
+        revealed_tiles: TilesArray,
+    },
+    LockedOut {
+        lock_start_time: UtcDateTime,
+        cause_tile: (usize, usize),
+        revealed_tiles: TilesArray,
+    },
+    Solved,
+}
+
+type TilesArray = [[bool; CELL_SIZE]; CELL_SIZE];
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_systems(Startup, (setup, spawn_fake_player).chain())
+        .add_systems(Startup, (setup, spawn_gamecursor).chain())
         .add_systems(
             Update,
             (
-                update_tilemap,
-                move_player,
-                log_tile,
+                // update_tilemap,
+                move_gamecursor,
+                move_cells,
+                // log_tile,
                 update_camera,
                 zoom_camera,
             )
@@ -36,19 +72,29 @@ fn main() {
         .run();
 }
 
-#[derive(Component, Deref, DerefMut)]
-struct UpdateTimer(Timer);
-
 #[derive(Resource, Deref, DerefMut)]
 struct SeededRng(ChaCha8Rng);
 
 fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     // We're seeding the PRNG here to make this example deterministic for testing purposes.
     // This isn't strictly required in practical use unless you need your app to be deterministic.
-    let mut rng = ChaCha8Rng::seed_from_u64(42);
+    let mut rng = ChaCha8Rng::seed_from_u64(RANDOM_SEED);
+
+    let minefield_tilemap_chunk: TilemapChunk = TilemapChunk {
+        chunk_size: UVec2::splat(16),
+        tile_display_size: UVec2::splat(16),
+        tileset: assets.load_with_settings(
+            "minefield-tiles.png",
+            |settings: &mut ImageLoaderSettings| {
+                // The tileset texture is expected to be an array of tile textures, so we tell the
+                // `ImageLoader` that our texture is composed of 4 stacked tile images.
+                settings.array_layout = Some(ImageArrayLayout::RowCount { rows: 16 });
+            },
+        ),
+        ..default()
+    };
 
     let chunk_size = UVec2::splat(16);
-    let tile_display_size = UVec2::splat(16);
     let tile_data: Vec<Option<TileData>> = (0..chunk_size.element_product())
         .map(|_| rng.random_range(0..15))
         .map(|i| {
@@ -59,23 +105,35 @@ fn setup(mut commands: Commands, assets: Res<AssetServer>) {
             }
         })
         .collect();
+    let mut initial_bomb_locations: TilesArray = [[false; CELL_SIZE]; CELL_SIZE];
+    for i in 1..CELL_SIZE {
+        initial_bomb_locations[i - 1][i - 1] = true;
+    }
 
     commands.spawn((
-        TilemapChunk {
-            chunk_size,
-            tile_display_size,
-            tileset: assets.load_with_settings(
-                "minefield-tiles.png",
-                |settings: &mut ImageLoaderSettings| {
-                    // The tileset texture is expected to be an array of tile textures, so we tell the
-                    // `ImageLoader` that our texture is composed of 4 stacked tile images.
-                    settings.array_layout = Some(ImageArrayLayout::RowCount { rows: 16 });
-                },
-            ),
-            ..default()
+        Transform {
+            ..Default::default()
         },
-        TilemapChunkTileData(tile_data),
-        UpdateTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        Cell {
+            logical_position: LogicalPosition { x: 0, y: 0 },
+            state: CellState::Fresh,
+            bomb_locations: initial_bomb_locations,
+        },
+        minefield_tilemap_chunk.clone(),
+        TilemapChunkTileData(tile_data.clone()),
+    ));
+
+    commands.spawn((
+        Transform {
+            ..Default::default()
+        },
+        Cell {
+            logical_position: LogicalPosition { x: 100, y: 0 },
+            state: CellState::Fresh,
+            bomb_locations: initial_bomb_locations,
+        },
+        minefield_tilemap_chunk.clone(),
+        TilemapChunkTileData(tile_data.clone()),
     ));
 
     commands.spawn(Camera2d);
@@ -85,11 +143,11 @@ fn setup(mut commands: Commands, assets: Res<AssetServer>) {
 
 /// Update the camera position by tracking the player.
 fn update_camera(
-    mut camera: Single<&mut Transform, (With<Camera2d>, Without<Player>)>,
-    player: Single<&Transform, (With<Player>, Without<Camera2d>)>,
+    mut camera: Single<&mut Transform, (With<Camera2d>, Without<GameCursor>)>,
+    game_cursor: Single<&Transform, (With<GameCursor>, Without<Camera2d>)>,
     time: Res<Time>,
 ) {
-    let Vec3 { x, y, .. } = player.translation;
+    let Vec3 { x, y, .. } = game_cursor.translation;
     let direction = Vec3::new(x, y, camera.translation.z);
 
     // Applies a smooth effect to camera movement using stable interpolation
@@ -99,10 +157,13 @@ fn update_camera(
         .smooth_nudge(&direction, CAMERA_DECAY_RATE, time.delta_secs());
 }
 
-#[derive(Component)]
-struct Player;
+#[derive(Component, Debug)]
+struct GameCursor {
+    logical_position: LogicalPosition,
+    float_position: Vec2,
+}
 
-fn spawn_fake_player(
+fn spawn_gamecursor(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -115,53 +176,52 @@ fn spawn_fake_player(
         Mesh2d(meshes.add(Rectangle::new(8., 8.))),
         MeshMaterial2d(materials.add(Color::from(RED_400))),
         transform,
-        Player,
-    ));
-
-    let mut transform = chunk.calculate_tile_transform(UVec2::new(5, 6));
-    transform.translation.z = 1.;
-
-    // second "player" to visually test a non-zero position
-    commands.spawn((
-        Mesh2d(meshes.add(Rectangle::new(8., 8.))),
-        MeshMaterial2d(materials.add(Color::from(RED_400))),
-        transform,
+        GameCursor {
+            logical_position: LogicalPosition { x: 0, y: 0 },
+            float_position: Vec2 { x: 0., y: 0. },
+        },
     ));
 }
 
-/// Update the player position with keyboard inputs.
-/// Note that the approach used here is for demonstration purposes only,
-/// as the point of this example is to showcase the camera tracking feature.
-///
-/// A more robust solution for player movement can be found in `examples/movement/physics_in_fixed_timestep.rs`.
-fn move_player(
-    mut player: Single<&mut Transform, With<Player>>,
+fn move_gamecursor(
+    mut game_cursor: Single<&mut GameCursor>,
     time: Res<Time>,
     kb_input: Res<ButtonInput<KeyCode>>,
 ) {
     let mut direction = Vec2::ZERO;
 
     if kb_input.pressed(KeyCode::KeyW) {
-        direction.y += 1.;
-    }
-
-    if kb_input.pressed(KeyCode::KeyS) {
         direction.y -= 1.;
     }
 
+    if kb_input.pressed(KeyCode::KeyS) {
+        direction.y += 1.;
+    }
+
     if kb_input.pressed(KeyCode::KeyA) {
-        direction.x -= 1.;
+        direction.x += 1.;
     }
 
     if kb_input.pressed(KeyCode::KeyD) {
-        direction.x += 1.;
+        direction.x -= 1.;
     }
 
     // Progressively update the player's position over time. Normalize the
     // direction vector to prevent it from exceeding a magnitude of 1 when
     // moving diagonally.
-    let move_delta = direction.normalize_or_zero() * PLAYER_SPEED * time.delta_secs();
-    player.translation += move_delta.extend(0.);
+    let move_delta = direction.normalize_or_zero() * GAMECURSOR_SPEED * time.delta_secs();
+
+    game_cursor.logical_position.x = game_cursor.logical_position.x
+        + (move_delta.x.trunc() as i64)
+        + (game_cursor.float_position.x.trunc() as i64);
+    game_cursor.logical_position.y = game_cursor.logical_position.y
+        + (move_delta.y.trunc() as i64)
+        + (game_cursor.float_position.y.trunc() as i64);
+
+    game_cursor.float_position.x = game_cursor.float_position.x.fract() + move_delta.x.fract();
+    game_cursor.float_position.y = game_cursor.float_position.y.fract() + move_delta.y.fract();
+
+    // game_cursor.translation += move_delta.extend(0.);
 }
 
 fn zoom_camera(
@@ -184,32 +244,38 @@ fn zoom_camera(
     }
 }
 
-fn update_tilemap(
-    time: Res<Time>,
-    mut query: Query<(&mut TilemapChunkTileData, &mut UpdateTimer)>,
-    mut rng: ResMut<SeededRng>,
+// fn spawn_cells(
+//     mut commands: Commands,
+//     game_cursor: Single<&GameCursor>,
+//     transform: Query<&Transform, With<Camera>>,
+//     windows: Query<&Window>,
+//     random: Res<SeededRng>,
+// ) {
+//     let Ok(window) = windows.single() else {
+//         return;
+//     };
+
+//     let window_size = window.size();
+
+//     // TODO: Actually Spawn Cells
+// }
+
+fn move_cells(
+    q_cells: Query<(&mut Transform, &Cell), With<Cell>>,
+    game_cursor: Single<&GameCursor>,
 ) {
-    for (mut tile_data, mut timer) in query.iter_mut() {
-        timer.tick(time.delta());
+    for (mut transform, cell) in q_cells {
+        let game_space_transform: Vec2 = Vec2 {
+            x: ((game_cursor.logical_position.x - cell.logical_position.x) as f32)
+                + game_cursor.float_position.x,
+            y: ((game_cursor.logical_position.y - cell.logical_position.y) as f32)
+                + game_cursor.float_position.y,
+        };
 
-        if timer.just_finished() {
-            for _ in 0..50 {
-                let index = rng.random_range(0..tile_data.len());
-                tile_data[index] = Some(TileData::from_tileset_index(rng.random_range(0..5)));
-            }
-        }
-    }
-}
+        transform.translation.x = game_space_transform.x;
+        transform.translation.y = game_space_transform.y;
+        transform.translation.z = 0.;
 
-// find the data for an arbitrary tile in the chunk and log its data
-fn log_tile(tilemap: Single<(&TilemapChunk, &TilemapChunkTileData)>, mut local: Local<u16>) {
-    let (chunk, data) = tilemap.into_inner();
-    let Some(tile_data) = data.tile_data_from_tile_pos(chunk.chunk_size, UVec2::new(3, 4)) else {
-        return;
-    };
-    // log when the tile changes
-    if tile_data.tileset_index != *local {
-        info!(?tile_data, "tile_data changed");
-        *local = tile_data.tileset_index;
+        println!("{}, {:?}", transform.translation, cell.logical_position);
     }
 }
